e6bf64a44022fb7619a0e5066ab851ea0030a8c3
juanrs25 <juanmanuelrodriguez@unicomfacauca.edu.co>
2025-03-28 09:52:25 -0500

    Commit 1

---
+++ inicio de sesion/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <title>Inicio de Sesion</title>
+</head>
+
+<body>
+
+    <form action="Iniciar Sesion.php" method="POST">
+        <h1 class="text text-center">INICIAR SESION</h1>
+        <hr>
+        <i class="fa-solid fa-user"></i>
+        <label>Usuario</label>
+        <input type="text" name="Usuario" placeholder="Nombre de usuario">
+
+        <i class="fa-solid fa-unlock"></i>
+        <label>Clave</label>
+        <input type="text" name="Clave" placeholder="Clave">
+        <hr>
+
+        <button type="sunmit">Iniciar Sesion</button>
+
+        <a href="CrearCuenta.php">Crear Cuenta</a>
+
+    </form>
+
+</body>
+
+</html>
